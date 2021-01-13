b4ddd346f93f61365fd68a35935f0ededc9814ef
rookiessf <562549535@qq.com>
2021-01-13 15:40:36 +0800

    模板

---
+++ miniprogram/components/zj_swiper/zj_swiper.js
@@ -0,0 +1,31 @@
+// components/zj_swiper/zj_swiper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    swiperImgUrl: [{
+      url: '../../images/banner/1.JPG'
+    }, {
+      url: '../../images/banner/2.JPG'
+    }, {
+      url: '../../images/banner/3.JPG'
+    }, {
+      url: '../../images/banner/4.JPG'
+    }]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
